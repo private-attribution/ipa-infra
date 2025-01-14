43cdbfc393638aa9c5bd5d113876d35009796313
Shinta Liem <shintaliem@meta.com>
2025-01-14 13:35:18 +0800

    Add prometheus as part of install

--- monitoring_uninstall.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# helm uninstall prometheus
-NAMESPACE_NAME="monitoring"
-MONITORING_RELEASE_NAME="monitoring"
-
-helm uninstall $MONITORING_RELEASE_NAME -n $NAMESPACE_NAME
--- uninstall.sh
+++ uninstall.sh
@@ -4,3 +4,9 @@
 helm uninstall h1
 helm uninstall h2
 helm uninstall h3
+
+# helm uninstall prometheus
+NAMESPACE_NAME="monitoring"
+MONITORING_RELEASE_NAME="monitoring"
+
+helm uninstall $MONITORING_RELEASE_NAME -n $NAMESPACE_NAME
